5bbaf9acab534eabab61ed515d07b41a4aae4604
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-02-14 15:38:23 +0100

    StrategyTransport met een Branch

--- StrategyTransport/src/be/vdab/Main.java
+++ StrategyTransport/src/be/vdab/Main.java
@@ -3,6 +3,6 @@ package be.vdab;
 public class Main {
     public static void main(String[] args) {
         Persoon ikke = new Persoon();
-        ikke.steekMeerOver(()->"Zeilen over het meer");
+        ikke.steekMeerOver(()->"Schaatsen");
     }
 }

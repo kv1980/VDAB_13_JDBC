eae2bffdd37f65e9565d0a20babdf7ab409a58f8
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-02-14 15:42:30 +0100

    StrategyTransport met een "Zwemmen op het meer"

--- StrategyTransport/src/be/vdab/Main.java
+++ StrategyTransport/src/be/vdab/Main.java
@@ -3,6 +3,6 @@ package be.vdab;
 public class Main {
     public static void main(String[] args) {
         Persoon ikke = new Persoon();
-        ikke.steekMeerOver(()->"Schaatsen");
+        ikke.steekMeerOver(()->"Zwemmen in het meer");
     }
 }

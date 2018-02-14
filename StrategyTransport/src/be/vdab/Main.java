e651afcaa7bb366c6d8124b3644d3543d95c0e88
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-02-14 15:40:51 +0100

    StrategyTransport met een "Schaatsen op het meer"

--- StrategyTransport/src/be/vdab/Main.java
+++ StrategyTransport/src/be/vdab/Main.java
@@ -3,6 +3,6 @@ package be.vdab;
 public class Main {
     public static void main(String[] args) {
         Persoon ikke = new Persoon();
-        ikke.steekMeerOver(()->"Schaatsen");
+        ikke.steekMeerOver(()->"Schaatsen op het meer");
     }
 }

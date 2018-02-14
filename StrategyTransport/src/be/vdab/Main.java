2448be4df4e39b9ce71c8b5555612e390f94b4ab
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-02-14 15:30:12 +0100

    StrategyTransport einde

--- StrategyTransport/src/be/vdab/Main.java
+++ StrategyTransport/src/be/vdab/Main.java
@@ -2,6 +2,7 @@ package be.vdab;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Persoon ikke = new Persoon();
+        ikke.steekMeerOver(()->"Zeilen over het meer");
     }
 }
---
+++ StrategyTransport/src/be/vdab/TransportMiddel.java
@@ -0,0 +1,5 @@
+package be.vdab;
+@FunctionalInterface
+public interface TransportMiddel {
+    String kiesTransportMiddel(); 
+}

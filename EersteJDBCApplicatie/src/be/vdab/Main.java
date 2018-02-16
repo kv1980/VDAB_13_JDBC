c5a941f7e69aab4d21c3e92e009bbd9820b53c8f
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-02-16 15:34:26 +0100

    EersteApplicatieJDBC

--- EersteJDBCApplicatie/src/be/vdab/Main.java
+++ EersteJDBCApplicatie/src/be/vdab/Main.java
@@ -6,11 +6,11 @@ import java.sql.SQLException;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "vdab";
+    private static final String USER = "cursist";
+    private static final String PASSWORD = "VDABcursist2018";
     public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection(URL,USER,PASSWORD)){
-            System.out.println("Connectie geopend.");
+            System.out.println("Connectie geopend."); //ZO KORT MOGELIJK !!!!!!!
         } catch (SQLException ex){
             System.err.println("Connectie niet geopend.");
         } 
